08d03390ca14f652330036c0c6a5b062b6cbfe23
Rentiii612 <yorentianarafu@gmail.com>
2025-07-22 14:33:06 +0700

    Menambahkan File UAS

---
+++ views/show-bahan.php
@@ -0,0 +1,4 @@
+<?php echo "Nama: " . $bahan['nama']; ?> <br>
+<?php echo "Jenis: " .  $bahan['jenis']; ?> <br>
+<?php echo "Deskripsi: " . $bahan['deskripsi']; ?> <br>
+<?php echo "Harga: " . $bahan['harga']; ?> <br>
